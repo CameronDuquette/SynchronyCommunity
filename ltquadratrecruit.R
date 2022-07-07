919f8eb754ecd28db020dd091cea664a13d037db
Cameron Duquette <cduquett@nmsu.edu>
2022-07-07 13:40:48 -0600

    Deal with multiple sampling events in early years by taking the non-growing season sampling events only

--- ltquadratrecruit.R
+++ ltquadratrecruit.R
@@ -1,5 +1,10 @@
 
 ###Reading in veg quadrat data
-
+library(tidyverse)
 quads=read.csv('F:/NMSU/Spatiotemporal Synchrony/JQPNoannuals15_79.csv')
+
+str(quads)
+
+jrn_quads_latest=quads%>%mutate(QYear=paste(quadrat, project_year, sep = "_"))%>%
+  group_by(QYear)%>%slice_max(month, with_ties = TRUE)%>%ungroup()
 

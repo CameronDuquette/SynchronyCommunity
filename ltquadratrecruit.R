e3bcf740fefb93d11b18a7cf1cc297d985fd3095
Cameron Duquette <cduquett@nmsu.edu>
2022-07-07 14:37:31 -0600

    boer and total abundance and cover through time

--- ltquadratrecruit.R
+++ ltquadratrecruit.R
@@ -5,6 +5,22 @@ quads=read.csv('F:/NMSU/Spatiotemporal Synchrony/JQPNoannuals15_79.csv')
 
 str(quads)
 
-jrn_quads_latest=quads%>%mutate(QYear=paste(quadrat, project_year, sep = "_"))%>%
-  group_by(QYear)%>%slice_max(month, with_ties = TRUE)%>%ungroup()
+jrn_quads_summed_total=quads%>%mutate(QYear=paste(quadrat, project_year, sep = "_"))%>%
+  group_by(QYear)%>%slice_max(month, with_ties = TRUE)%>%filter(project_year<=1979)%>%
+  mutate(spsum=sum(area, na.rm = TRUE),absum=sum(Abundance, na.rm=TRUE))%>%slice_head()%>%ungroup()
 
+
+###total cover vs total abundance over time
+ggplot(jrn_quads_summed_total, aes(x=project_year))+geom_smooth(aes(y=scale(spsum)), fill='blue')+
+  geom_smooth(aes(y=scale(absum)), fill='red')+theme_classic()
+
+
+###boer cover vs abundance over time
+jrn_quads_summed_BOER=quads%>%mutate(QYear=paste(quadrat, project_year, sep = "_"))%>%
+  group_by(QYear)%>%slice_max(month, with_ties = TRUE)%>%filter(project_year<=1979&species_code=='BOER4')%>%
+  mutate(spsum=sum(area, na.rm = TRUE),absum=sum(Abundance, na.rm=TRUE))%>%slice_head()%>%ungroup()
+
+
+###total cover vs total abundance over time
+ggplot(jrn_quads_summed_BOER, aes(x=project_year))+geom_smooth(aes(y=scale(spsum)), fill='blue')+
+  geom_smooth(aes(y=scale(absum)), fill='red')+theme_classic()
